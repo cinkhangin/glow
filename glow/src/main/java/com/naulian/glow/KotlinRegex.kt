e01bb8a2e5e89d539590d49f40a27523595e0b0f
Naulian <cinkhangin@gmail.com>
2023-04-07 10:23:49 +0630

    release 0.0.5

--- glow/src/main/java/com/naulian/glow/KotlinRegex.kt
+++ glow/src/main/java/com/naulian/glow/KotlinRegex.kt
@@ -7,10 +7,10 @@ object KotlinRegex {
     val variables = "(val|var)\\s+(\\w+)".toRegex()
     val strings = "\"[^\"]*\"|'[^']*'".toRegex()
     val numbers = "\\b(\\d+(\\.\\d+)?([fF])?|0x[\\da-fA-F]+|\\d+([lL]))\\b".toRegex()
-    val methods = "\\b[a-z]\\w*\\(\\)".toRegex()
+    //val methods = "\\b[a-z]\\w*\\(\\)".toRegex()
     //val properties = "[^\\.]*\\.([^\\.]*)".toRegex()
     val comments = "//[^\n]*".toRegex()
     val documentations = "/\\*[\\s\\S]*?\\*/".toRegex()
 
-    val instanceProperty = Regex("(?<=\\.)[a-z][\\w\\s)},\\]]*\n")
+    val instanceProperty = Regex("(?<=\\.)[a-z][a-zA-Z\\d]*?(?=[\\s,)\\].])")
 }
---
+++ glow/src/test/java/com/naulian/glow/RegexTest.kt
@@ -0,0 +1,11 @@
+package com.naulian.glow
+
+import org.junit.Test
+
+class RegexTest {
+
+    @Test
+    fun instancePropertyTest() {
+
+    }
+}
